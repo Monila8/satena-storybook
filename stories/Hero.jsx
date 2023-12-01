275a024152867b49dc0abf91f0205953f1101da5
Monila8 <128505931+Monila8@users.noreply.github.com>
2023-12-01 01:44:02 +0000

    add hero pixel perfect

--- stories/Hero.jsx
+++ stories/Hero.jsx
@@ -1,32 +1,23 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { Button } from "./Button";
-import Traveller from "./assets/Traveller.png"
-import './hero.css'
+import "./hero.css";
 
 export const Hero = ({}) => {
   return (
     <section className="hero">
-      <div className="hero__container">
-        <div className="hero__content">
-          <h1 className="hero__headline">
-          Vuelve a volar
-          <br />
-          en cielos
-          <br />
-          claros y sin
-          <br />
-          turbulencias
-          </h1>
-          <p className="hero__subtitle">
-          CONOCE MÁS SOBRE NUESTRAS POLÍTICAS PENSADAS PARA TI{" "} <br />
-          </p>
-          <Button label="Conoce más aquí" primary size="large" />
+        <div className="hero__container">
+          <div className="hero__content">
+            <h1 className="hero__headline">
+              Vuelve a volar en cielos claros y sin turbulencias
+            </h1>
+
+            <p className="hero__subtitle">
+              Conoce más sobre nuestras políticas pensadas para ti
+            </p>
+            <Button label="Conoce más aquí" primary size="large" />
+          </div>
         </div>
-        <figure className="hero__figure">
-          <img src={Traveller} alt="" className="hero__image" />
-        </figure>
-      </div>
     </section>
   );
 };
--- stories/Hero.stories.js
+++ stories/Hero.stories.js
@@ -1,10 +1,22 @@
+import { Preview } from '@storybook/react';
 import { Hero } from "./Hero";
+
+const customViewports = {
+  desktop: {
+    name: 'desktop name',
+    styles: {
+      width: '1400px',
+      height: '963px',
+    },
+  },
+};
 
 export default {
   title: 'Components/Hero',
   component: Hero,
   parameters: {
     layout: 'centered',
+    viewport: { viewports: customViewports },
   },
 
   tags: ['autodocs'],
